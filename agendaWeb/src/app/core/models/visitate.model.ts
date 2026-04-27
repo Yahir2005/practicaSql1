8045265076883d4943cc6bb889076c587a6ec71a
Yahir2005 <luisyajairo@outlook.com>
2026-04-26 21:04:30 -0600

    visitante model

---
+++ agendaWeb/src/app/core/models/visitate.model.ts
@@ -0,0 +1,9 @@
+export interface Visitante {
+    idVisitante: number;
+    nombre: string;
+    apellidos: string;
+    telefono: string;
+    email: string;
+    fechaCreacion: Date;
+    activo: boolean;
+}
