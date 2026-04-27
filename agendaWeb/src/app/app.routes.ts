779ff282a11ceb0e489257f375a75776f393db2b
Yahir2005 <luisyajairo@outlook.com>
2026-04-26 20:30:48 -0600

    router cita

--- agendaWeb/src/app/app.routes.ts
+++ agendaWeb/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+    },
+    {
+        path: 'citas',
+        loadChildren: () => import('./features/citas/cita.component').then(m => m.CitaComponent)
+    }
+];
